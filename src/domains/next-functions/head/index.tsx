316c92b3cf540c4ae99c9b3112e7157d24d9c17d
KwonCheulJin <chkftm12@gmail.com>
2024-11-20 10:42:31 +0900

    feat: next/head 실습 완료

---
+++ src/domains/next-functions/head/index.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head';
+
+export default function HeadMain() {
+  return (
+    <main>
+      <Head>
+        <title>My page title</title>
+        <meta name="description" content="My page description" />
+      </Head>
+    </main>
+  );
+}
---
+++ src/pages/next-functions/head/index.tsx
@@ -0,0 +1,5 @@
+import HeadMain from '@/domains/next-functions/head';
+
+export default function HeadPage() {
+  return <HeadMain />;
+}
